f21320752fe05320f8e0d4db0c43f9bd363b029a
mukundjha-mj <mukundjha204@gmail.com>
2025-07-22 21:42:12 +0530

    Phase 1: content endpoint done

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const JWT_SECRET: string="mukundjha204@secret1234"
+const DATABASE_URL: string="mongodb+srv://mukundjha204:sVbuyUP6wSOCsO0G@cluster0.srrhil7.mongodb.net/secondBrain"
+export{
+    JWT_SECRET,
+    DATABASE_URL
+}
