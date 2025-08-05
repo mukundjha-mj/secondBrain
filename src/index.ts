911961b770fb418f50b4c92356ecf6bfe1e11955
mukundjha-mj <mukundjha204@gmail.com>
2025-08-05 12:28:38 +0530

    Phase1: Add keep alive for service to prevent cold starts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const url = [
     "http://localhost:5173",
-    "https://cb67fqtt-5173.inc1.devtunnels.ms"]
+    "https://second-brain-frontend-six.vercel.app/"]
 
 app.use(cors({
 
